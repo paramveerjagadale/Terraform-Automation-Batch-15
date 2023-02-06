5eab9b56234f0a2f3c515f9e4f35efc9028899c7
paramveerjagadale <96696888+paramveerjagadale@users.noreply.github.com>
2023-02-06 14:51:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-5"
+    bucket = "mydev-tf-state-bucket-b15"
     key = "main"
-    region = "ap-south-1"
+    region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
